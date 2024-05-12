986c6d24f5be98def92ecb919bdd3f7bb5f314cb
YASSER Korzane <ly_korzane@esi.dz>
2024-05-12 09:02:43 +0100

    adding static names to define nessasery files that will be used to save data

--- src/main/java/com/example/patient_management_system/HelloApplication.java
+++ src/main/java/com/example/patient_management_system/HelloApplication.java
@@ -19,11 +19,25 @@ public class HelloApplication extends Application {
     public static final OrthophonisteModel orthophonisteModel = new OrthophonisteModel(new OrthophonisteFileDataBase() );
 
 
+    public static final String usersDirectoryName = "orthophoniste_directory";
+    public static final String bilonDirectoryName = "bilons_directory";
+    public static final String fichSuivisDirectoryName = "fichSuivis_directory";
+
+
+
+
     //Files Names for each Speech Therapist
     public static final String  TherapistDBuserName = "SpeachTherapistDBFile.dat";
-    public static final String usersDirectoryName = "orthophoniste_directory";
     public static final String patientsDBFileName = "patientsFileDatabase.dat";
     public static final String categoryDbFileName = "rendez_vousDBFile.dat";
+    public static final String boDBFileName = "bilonsDBFile.dat";
+    public static final String observationCliniqueDBFileName = "observationCliniqueDBFile.dat";
+    public static final String testsDBFileName = "testsDBFile.dat";
+    public static final String questionsDBFileName = "questionsDBFile.dat";
+    public static final String fichSuivisDBFileName = "fichSuivisDBFile.dat";
+    public static final String objectifsDBFileName = "objectifsDBFile.dat";
+
+
     public static String currentUserName = null;
 
     @Override
